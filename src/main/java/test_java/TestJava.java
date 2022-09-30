3208cae80a6113fbd9b530a134b3d34d18d422f1
hoenninger <karl@hoenninger.at>
2022-09-30 12:44:19 +0000

    fix import for Vertx

--- src/main/java/test_java/TestJava.java
+++ src/main/java/test_java/TestJava.java
@@ -1,12 +1,12 @@
 package test_java;
 
-import io.vertx.Vertx;
+import io.vertx.core.Vertx;
 
 public class TestJava {
     public static void main(String[] args) {
 
         var vertx = Vertx.vertx();
         System.out.println("Vertx instance created in Test-Java");
-        
+
     }
 }
