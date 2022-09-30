35bbf68a626ea42e2bf9d82b2dcac4353276c4f7
hoenninger <karl@hoenninger.at>
2022-09-30 15:49:23 +0000

    set M2_REPO environment variable

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -2,6 +2,8 @@ FROM gitpod/workspace-full
 
 USER gitpod
 
+ENV M2_REPO=/workspace/m2-repository
+
 RUN bash -c ". /home/gitpod/.sdkman/bin/sdkman-init.sh && \
     sdk install java 17.0.4.1-tem && \
     sdk default java 17.0.4.1-tem && \
--- src/main/kotlin/test_kotlin/TestKotlin.kt
+++ src/main/kotlin/test_kotlin/TestKotlin.kt
@@ -2,11 +2,13 @@ package test_kotlin
 
 import io.vertx.core.Vertx
 
-class TestKotlin {
+fun main(args: Array<String>) {
 
-    fun main(args: Array<String>) {
+    val vertx = Vertx.vertx()
+    println("Vertx instance created in TestKotlin")
+}
 
-        val vertx = Vertx.vertx()
-        println("Vertx instance created in TestKotlin")
-    }
+class TestKotlin1 {
+
+
 }
