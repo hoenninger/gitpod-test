87ddbce2bea7fddc6e09e5bfaeb482ef1817c76c
hoenninger <karl@hoenninger.at>
2022-09-30 12:37:18 +0000

    initial commit

---
+++ src/main/java/test_java/TestJava.java
@@ -0,0 +1,12 @@
+package test_java;
+
+import io.vertx.Vertx;
+
+public class TestJava {
+    public static void main(String[] args) {
+
+        var vertx = Vertx.vertx();
+        System.out.println("Vertx instance created in Test-Java");
+        
+    }
+}
---
+++ src/main/kotlin/test_kotlin/TestKotlin.kt
@@ -0,0 +1,12 @@
+package test_kotlin
+
+import io.vertx.Vertx
+
+class TestKotlin {
+
+    fun main(args: Array<String>) {
+
+        Vertx.vertx()
+        println("Test-Java");
+    }
+}
